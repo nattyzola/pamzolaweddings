bd77ece31e9da4a5925af724f293423aae2d9fae
Natty Zola <natty@everlater.com>
2014-07-20 17:04:32 -0600

    initial commit, project set up and work

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+	def index
+
+	end
+
+	def about
+
+	end
+
+	def services
+
+	end
+
+	def inspiration
+
+	end
+
+	def contact
+
+	end
+
+	def free_wedding_planning_tools
+
+	end
+end
