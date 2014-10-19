6129692741388f985eff1f3d7c5f693badad3bb0
Natty Zola <natty@everlater.com>
2014-10-18 21:59:42 -0600

    adding a responsive header

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'index' => 'pages#index'
+  get 'home' => 'pages#home'
   get 'about' => 'pages#about'
   get 'services' => 'pages#services'
   get 'contact' => 'pages#contact'
